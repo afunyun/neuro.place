2931dffdc77ca8f55d2abbdb6c9016aa0f3d06d1
afunyun <afunyun@gmail.com>
2025-07-07 23:05:25 -0500

    the pain is immense

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/**/*.html",
-    "./public/**/*.js",
-    "./src/**/*.js",
-    "./timer/**/*.html",
-    "./timer/**/*.js"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		"./public/**/*.html",
+		"./public/**/*.js",
+		"./src/**/*.js",
+		"./timer/**/*.html",
+		"./timer/**/*.js",
+	],
+	theme: {
+		fontFamily: {
+			sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
+			mono: [
+				"ui-monospace",
+				"SFMono-Regular",
+				"Menlo",
+				"Monaco",
+				"Consolas",
+				"monospace",
+			],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
